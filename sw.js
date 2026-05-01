d1cf3f881095a14420a913abd5fcc6eb7b9ddcbd
akashvarma2 <akash.risma@gmail.com>
2026-05-01 08:13:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
 const CACHE_NAME = 'pulp-pro-v1';
 const ASSETS = [
-  '/Pulp/',
-  '/Pulp/index.html',
-  '/Pulp/edited-image.png',
-  '/Pulp/manifest.json',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'
+  './',
+  './index.html',
+  './manifest.json',
+  './defects.json',
+  './edited-image.png'
 ];
 
-// Install Event: Caches the essential files
+// Install event: Caches basic files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Pulp Pro: Caching Shell Assets');
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Activate Event: Cleans up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch Event: Serves assets from cache if offline
+// Fetch event: Required by Chrome for the "Install" button to appear
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
