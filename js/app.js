bf4a71005d4d431d77b4c65a88de5e2bafc2dc13
akashvarma2 <akash.risma@gmail.com>
2026-05-02 10:57:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,5 @@
-import './bridge.js';
-import { renderHistory, renderFavorites } from './ui.js';
-
-/* -------------------------
-   INITIAL BOOT
---------------------------*/
+import { initApp } from './core.js';
 
 window.addEventListener('load', () => {
-    const savedTheme = localStorage.getItem('pulpTheme');
-
-    if (savedTheme === 'light') {
-        document.body.classList.add('light-theme');
-        document.getElementById('themeText').innerText = 'Light Mode';
-    }
-
-    renderHistory();
-    renderFavorites();
-
-    setTimeout(() => {
-        document.body.classList.add('loaded');
-        const input = document.getElementById('codeIn');
-        if (input) input.focus();
-    }, 2600);
+    initApp();
 });
