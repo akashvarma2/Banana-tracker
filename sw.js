479a423b608bb315bd82f9e02e537cca92b539dc
akashvarma2 <akash.risma@gmail.com>
2026-05-02 09:28:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ const ASSETS = [
   '/Pulp/banana.png',
   '/Pulp/mango.png',
   '/Pulp/avocado.png',
+  '/Pulp/rotten.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'
 ];
