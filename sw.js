ef761947e84923f6d0fdf13677dfa491c0b64dd5
akashvarma2 <akash.risma@gmail.com>
2026-05-02 02:42:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
+const CACHE_NAME = 'pulp-pro-v1';
 const ASSETS = [
   'index.html',
   'style.css',
   'js/master-logic.js',
   'edited-image.png',
-  'banana.png',
-  'mango.png',      // Added this
-  'avocado.png'     // Added this
+  'banana.png'
 ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
