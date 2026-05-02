c0d2ad641e924921393fd65abf087914a9cc2c33
akashvarma2 <akash.risma@gmail.com>
2026-05-02 09:13:16 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const ASSETS = [
-  'index.html',
-  'style.css',
-  'js/master-logic.js',
-  'edited-image.png',
-  'banana.png',
-  'mango.png',      // Added this
-  'avocado.png'     // Added this
-];
